d85945342024546b6f79a57037675466f759cb64
serkancan <serkancan@DESKTOP-9CS0Q1K>
2024-01-30 01:52:59 +0300

    otomatik cikis

--- instagram_bot/instagrambot.py
+++ instagram_bot/instagrambot.py
@@ -14,9 +14,12 @@ kullanıcı.send_keys('battaldenemebot1')
 sifre.send_keys('Heycorc123')
 browser.find_element_by_xpath('xpath_adı_giris_yap').click()
 
+time.sleep(10)
 
-
-
+browser.get('https://www.instagram.com/battaldenemebot1/')
+time.sleep(1)
+browser.find_element_by_xpath('ayalar_xpath').click()
+browser.find_element_by_xpath('cikis_yap_xpath').click()
 
 
 
