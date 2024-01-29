5f57f8a672472af0e6a9c256a82d773b7041fa67
serkancan <serkancan@DESKTOP-9CS0Q1K>
2024-01-30 02:00:16 +0300

    instagram bot takip ettirmek

--- instagram_bot/instagrambot.py
+++ instagram_bot/instagrambot.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
 import time
 
+print("""
+Instagram Bot
+Hos Geldiniz!
+
+1) Takip
+
+2) Begeni
+
+
+""")
+secim = input("Giriniz: ")
+takipkontrol = 0
+if secim=="1":
+    ad = input("Kullanıcı adı giriniz: ")
+    takipkontrol=1
+
+
 path = "c:\\Users\efsan\OneDrive\Masaüstü\İnstagrambot\geckodriver.exe"
 browser = webdriver.Firefox(executable_path=path)
 
@@ -13,6 +30,11 @@ sifre = browser.find_element_by_xpath("chropath_x_path_adı_sifre")
 kullanıcı.send_keys('battaldenemebot1')
 sifre.send_keys('Heycorc123')
 browser.find_element_by_xpath('xpath_adı_giris_yap').click()
+time.sleep(2)
+if takipkontrol==1:
+    browser.get("https://www.intagram.com/"+ad)
+    time.sleep(2)
+    browser.find_element_by_xpath("xpath_takip_et").click()
 
 time.sleep(10)
 
